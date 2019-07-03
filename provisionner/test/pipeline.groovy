08c56f26466e280e85166a3fb86dab62a401bd48
Yanis Guenane <yanis@guenane.org>
2019-07-03 09:24:23 +0200

    introduce pipeline

---
+++ provisionner/test/pipeline.groovy
@@ -0,0 +1,28 @@
+pipeline {
+
+    agent none
+
+    parameters {
+        string(
+            name: 'RHEL_COMPOSE_ID',
+            description: 'RHEL Compose Id to test',
+            defaultValue: 'TEST'
+        )
+    }
+
+    options {
+        timestamps()
+        buildDiscarder(logRotator(daysToKeepStr: '30'))
+    }
+
+    stages {
+
+        stage ('Pipeline Information') {
+            steps {
+                echo "Compose ID: ${params.RHEL_COMPOSE_ID}"
+            }
+        }
+
+    }
+
+}
